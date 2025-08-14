1e3ef446408bc921a744601c04494d443bf11eec
Malcolm McNeely <malcolm>
2025-08-14 20:30:20 +0100

    move class

--- src/EventStore.Blazor.EFCore.Postgres/Models/EventModel.cs
+++ src/EventStore.Blazor.EFCore.Postgres/Models/EventModel.cs
@@ -1,3 +1,3 @@
-﻿namespace EventStore.Blazor.EFCore.Postgres.Components.Models;
+﻿namespace EventStore.Blazor.EFCore.Postgres.Models;
 
 public record EventModel(int Id, string Stream, DateTime Time, string EventType, string Content);
