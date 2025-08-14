02cac898b698152f6d8707beb7fd7bea841c27bc
Malcolm McNeely <malcolm>
2025-08-14 21:17:20 +0100

    added placeholder for ef core command audit log

--- src/EventStore.EFCore.Postgres/Commands/CommandAudit.cs
+++ src/EventStore.EFCore.Postgres/Commands/CommandAudit.cs
@@ -1,8 +1,12 @@
-﻿using EventStore.Commands.Dispatching;
+﻿using EventStore.Commands;
+using EventStore.Commands.Dispatching;
 
-namespace EventStore.Azure.Commands;
+namespace EventStore.EFCore.Postgres.Commands;
 
 public class CommandAudit : ICommandAudit
 {
-    
+    public Task PublishAsync<T>(T command, CancellationToken token) where T : ICommand
+    {
+        throw new NotImplementedException();
+    }
 }
