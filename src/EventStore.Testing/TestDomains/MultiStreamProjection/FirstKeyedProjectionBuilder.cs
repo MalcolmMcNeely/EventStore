5c9f4c52e47820ee8b3a0a4ed276b6880ad1b048
Malcolm McNeely <malcolm>
2025-08-20 23:36:56 +0100

    fixed test domain refactor

--- src/EventStore.Testing/TestDomains/MultiStreamProjection/FirstKeyedProjectionBuilder.cs
+++ src/EventStore.Testing/TestDomains/MultiStreamProjection/FirstKeyedProjectionBuilder.cs
@@ -1,7 +1,6 @@
 ﻿using EventStore.Events.Streams;
 using EventStore.ProjectionBuilders;
 using EventStore.Projections;
-using EventStore.Testing.TestDomains.Simple;
 
 namespace EventStore.Testing.TestDomains.MultiStreamProjection;
 
@@ -9,10 +8,10 @@ public class FirstKeyedProjectionBuilder : ProjectionBuilder<FirstKeyedProjectio
 {
     public FirstKeyedProjectionBuilder(IProjectionRepository<FirstKeyedProjection> repository, IEventStreamFactory eventStreamFactory) : base(repository, eventStreamFactory)
     {
-        Handles<SimpleEvent>(x => x.Id, OnEvent);
+        Handles<MultiStreamProjectionEvent>(x => x.Id, OnEvent);
     }
 
-    void OnEvent(SimpleEvent @event, FirstKeyedProjection projection)
+    void OnEvent(MultiStreamProjectionEvent @event, FirstKeyedProjection projection)
     {
         projection.Data = @event.Data;
     }
--- src/EventStore.Testing/TestDomains/MultiStreamProjection/SecondKeyedProjectionBuilder.cs
+++ src/EventStore.Testing/TestDomains/MultiStreamProjection/SecondKeyedProjectionBuilder.cs
@@ -1,7 +1,6 @@
 ﻿using EventStore.Events.Streams;
 using EventStore.ProjectionBuilders;
 using EventStore.Projections;
-using EventStore.Testing.TestDomains.Simple;
 
 namespace EventStore.Testing.TestDomains.MultiStreamProjection;
 
@@ -9,10 +8,10 @@ public class SecondKeyedProjectionBuilder : ProjectionBuilder<SecondKeyedProject
 {
     public SecondKeyedProjectionBuilder(IProjectionRepository<SecondKeyedProjection> repository, IEventStreamFactory eventStreamFactory) : base(repository, eventStreamFactory)
     {
-        Handles<SimpleEvent>(x => x.Id, OnEvent);
+        Handles<MultiStreamProjectionEvent>(x => x.Id, OnEvent);
     }
 
-    void OnEvent(SimpleEvent @event, SecondKeyedProjection projection)
+    void OnEvent(MultiStreamProjectionEvent @event, SecondKeyedProjection projection)
     {
         projection.Data = @event.Data;
     }
